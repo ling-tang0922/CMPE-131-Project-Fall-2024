d24dec873425994f08ee85ed1cf4fc50960e9e93
anthony-cuellar <anthony.cuellar@sjsu.edu>
2024-10-30 17:45:28 -0700

    Add icons to login page, new fields, and welcome message

--- online-banking-system/src/files/CustomerLogin.js
+++ online-banking-system/src/files/CustomerLogin.js
@@ -1,24 +1,63 @@
-import React from "react";
-import WindowWrappper from "../components/WindowWrapper";
+import React, { useState } from "react";
+import {
+  CiUser,
+  CiLock,
+  CiMail,
+  CiSquareChevRight,
+  CiSquareChevLeft,
+} from "react-icons/ci";
+import WindowWrapper from "../components/WindowWrapper";
 import {
   Input,
   Label,
   Flex,
   Button,
-  ScrollView,
   Tabs,
+  Divider,
+  PhoneNumberField,
 } from "@aws-amplify/ui-react";
 import NavBar from "../components/NavBar";
-const CustomerLogin = () => {
-  return (
-    <WindowWrappper>
-      <NavBar></NavBar>
 
-      <div style={{ flex: 1, backgroundColor: "white" }}>
+const welcomeMSG = (
+  <>
+    <h1 style={{ textAlign: "center", marginTop: "10%", marginBottom: "0" }}>
+      Welcome Back!
+    </h1>
+    <p style={{ textAlign: "center", marginTop: "0%" }}>
+      Please enter your details to access your account.
+    </p>
+  </>
+);
+
+const getStartedMSG = (
+  <>
+    <h1 style={{ textAlign: "center", marginTop: "10%", marginBottom: "0" }}>
+      Get Started!
+    </h1>
+    <p style={{ textAlign: "center", marginTop: "0%" }}>
+      Please fill out your information to get started.
+    </p>
+  </>
+);
+
+const CustomerLogin = () => {
+  const [activeTab, setActiveTab] = useState("Login");
+  return (
+    <WindowWrapper>
+      <NavBar></NavBar>
+      <div
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          overflow: "hidden",
+          height: "100vh",
+        }}
+      >
+        {activeTab === "Login" ? welcomeMSG : getStartedMSG}
         <div
           style={{
             margin: "10%",
-            marginTop: "12%",
+            marginTop: "0%",
             minWidth: "250px",
             backgroundColor: "white",
             boxShadow: "0 0 10px rgba(76, 175, 80, 0.5)",
@@ -26,6 +65,7 @@ const CustomerLogin = () => {
             justifyContent: "center",
             marginRight: "30%",
             marginLeft: "30%",
+            viewport: "width=device-width, initial-scale=1",
           }}
         >
           <Tabs
@@ -33,66 +73,246 @@ const CustomerLogin = () => {
             justifyContent="flex-start"
             indicatorPosition="top"
             defaultValue="Login"
+            onValueChange={(value) => setActiveTab(value)}
             items={[
               {
                 label: "Login",
                 value: "Login",
                 content: (
                   <div style={{ padding: "10px" }}>
-                    <Flex direction="column" gap="small">
+                    <Flex direction="column" gap="default">
                       <Label htmlFor="first_name">Username:</Label>
-                      <Input placeholder="Enter Username" name="username" />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiUser
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "3px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          placeholder="Enter Username"
+                          name="username"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
                       <Label htmlFor="first_name">Password:</Label>
-                      <Input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="username"
-                      />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiLock
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "3px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          type="password"
+                          placeholder="Enter Password"
+                          name="username"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
+
                       <Button variation="primary" colorTheme="success">
                         Login
                       </Button>
                     </Flex>
-                    <a
-                      style={{
-                        color: "blue",
-                        display: "flex",
-                        justifyContent: "end",
-                        margin: "10px 0",
-                        fontSize: "small",
-                      }}
-                      href="/"
+
+                    <Flex
+                      direction="row"
+                      gap="small"
+                      style={{ marginTop: "3%", justifyContent: "flex-end" }}
                     >
-                      Forgot Password?
-                    </a>
+                      <a href="/" style={{ fontSize: 15 }}>
+                        Forgot Username?
+                      </a>
+                      <Divider size="default" orientation="vertical" />
+                      <a href="/" style={{ fontSize: 15 }}>
+                        Forgot Password?
+                      </a>
+                    </Flex>
                   </div>
                 ),
               },
               {
                 label: "Sign Up",
-                value: "Sign Up",
+                value: "SignUp",
                 content: (
                   <div style={{ padding: "10px" }}>
                     <Flex direction="column" gap="small">
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <div style={{ width: "49%" }}>
+                          <Label htmlFor="first_name">Fast Name:</Label>
+                          <div
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              position: "relative",
+                            }}
+                          >
+                            <CiSquareChevRight
+                              style={{
+                                marginLeft: "10px",
+                                marginBottom: "2px",
+                                fontSize: "1.3rem",
+                                position: "absolute",
+                              }}
+                            />
+                            <Input
+                              placeholder="Enter First Name"
+                              name="firstName"
+                              style={{ paddingLeft: "35px" }}
+                            />
+                          </div>
+                        </div>
+
+                        <div style={{ width: "49%" }}>
+                          <Label htmlFor="last_name">Last Name:</Label>
+                          <div
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              position: "relative",
+                            }}
+                          >
+                            <CiSquareChevRight
+                              style={{
+                                marginLeft: "10px",
+                                marginBottom: "2px",
+                                fontSize: "1.3rem",
+                                position: "absolute",
+                              }}
+                            />
+                            <Input
+                              placeholder="Enter Last Name"
+                              name="lastName"
+                              style={{ paddingLeft: "35px" }}
+                            />
+                          </div>
+                        </div>
+                      </div>
                       <Label htmlFor="first_name">Username:</Label>
-                      <Input placeholder="Enter Username" name="username" />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiUser
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "3px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          placeholder="Enter Username"
+                          name="username"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
                       <Label htmlFor="first_name">Password:</Label>
-                      <Input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="username"
-                      />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiLock
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "3px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          type="password"
+                          placeholder="Enter Password"
+                          name="password"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
+
                       <Label htmlFor="first_name">Confirm Password:</Label>
-                      <Input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="username"
-                      />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiLock
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "3px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          type="password"
+                          placeholder="Confirm Password"
+                          name="confirmPassword"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
                       <Label htmlFor="first_name">Email:</Label>
-                      <Input placeholder="Enter Username" name="username" />
-                      <Label htmlFor="first_name">Phone Number:</Label>
-                      <Input placeholder="Enter Phone Number" name="username" />
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          position: "relative",
+                        }}
+                      >
+                        <CiMail
+                          style={{
+                            marginLeft: "10px",
+                            marginBottom: "1px",
+                            fontSize: "1.3rem",
+                            position: "absolute",
+                          }}
+                        />
+                        <Input
+                          placeholder="bank@Ebankify.com"
+                          name="email"
+                          style={{ paddingLeft: "35px" }}
+                        />
+                      </div>
+
+                      <PhoneNumberField
+                        defaultDialCode="+1"
+                        label="Phone number:"
+                        placeholder="234-567-8910"
+                        errorMessage="Enter a Valid Phone Number"
+                      />
                       <Button variation="primary" colorTheme="success">
-                        Sign Up 
+                        Sign Up
                       </Button>
                     </Flex>{" "}
                   </div>
@@ -102,7 +322,7 @@ const CustomerLogin = () => {
           />
         </div>
       </div>
-    </WindowWrappper>
+    </WindowWrapper>
   );
 };
 export default CustomerLogin;
